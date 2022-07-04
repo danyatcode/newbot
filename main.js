80eed5b95838d2fad929cc9ceac812d102ab0dbd
danyatcode <danyatd22@gmail.com>
2022-07-04 18:33:54 +0300

    summary

---
+++ main.js
@@ -0,0 +1,66 @@
+const Discord = require('discord.js') // подключение библиотеки                  Видео про бота https://youtu.be/1lzPIhTaPDY
+const client = new Discord.Client() // создание клиента
+
+client.on('ready', () =>{ // ивент, когда бот запускается https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-ready
+    client.generateInvite("ADMINISTRATOR").then(invite => console.log(`Ссылка на добавление ${invite}`))
+    console.log(`Привет! ${client.user.tag} запустился!`) // информация в консоль про успешный запуск
+})
+
+client.on('message', message =>{ // ивент, когда приходит любое сообщение в чат https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-message
+    if (message.author.bot) return; // если автор сообщения - бот, ничего не происходит 
+    if (message.content == '!профиль') { // если пользователь написал "!профиль" 
+    let embed = new Discord.MessageEmbed() // создание ембед сообщения
+    .setTitle(message.author.username) // в тайтле имя автора 
+    let status = ''
+    switch (message.author.presence.status) { // проверка статусов 
+        case 'online':
+            status = 'онлайн'; break; 
+            case 'idle':
+                status = ':orange_circle:нет на месте'; break;
+                case 'offline':
+                    status = 'нет в сети'; break;
+                    case 'dnd':
+                        status = ':red_circle:не беспокоить'; break;
+    }
+    embed.setDescription(`**Ваш дискорд айди: ${message.author.id}
+    Ваш статус: ${status}
+    Дата создания аккаунта: ${message.author.createdAt.toLocaleDateString()}
+    Дата входа на сервер: ${message.member.joinedAt.toLocaleDateString()}
+    **`) // описание ембеда
+    .setColor('RANDOM') // рандомный цвет ембеда
+    .setThumbnail(message.author.avatarURL()) // вставляем в ембед аватарку пользователя
+    message.channel.send(embed) // отправляем сообщение в канал где была написана команда   
+    }
+    if (message.content == '!stats') { // если пользователь написал "!профиль" 
+        message.channel.send('Пішов нахуй') // отправляем сообщение в канал где была написана команда
+        let role = message.guild.roles.cache.find(r => r.id === "993190930687475792");
+        // Or add it to yourself
+        message.author.roles.add(role);   
+        }
+})
+
+client.on('messageDelete', message =>{ // ивент, когда удаляется любое сообщение с сервера https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildMemberAdd
+    let embed = new Discord.MessageEmbed()
+    .setTitle('Было удалено сообщение!')
+    .setColor('RANDOM')
+    .addField(`Удалённое сообщение:`, message.content, true)
+    .addField("Автор:",`${message.author.tag} (${message.author})`,true)
+    .addField("Канал:", `${message.channel}`, false)
+    .setFooter(' - ',`${message.author.avatarURL()}`)
+    .setTimestamp(message.createdAt);
+  client.channels.cache.get("АЙДИ КАНАЛА С ЛОГАМИ").send(embed); // айди вашего канала с логами
+})
+
+
+async function change() {
+    let members = client.guilds.cache.get("АЙДИ ВОЙСА").memberCount // сколько людей на сервере + указать айди своего сервера
+    client.channels.cache.get("АЙДИ СЕРВЕРА").setName(`На сервере: ${members}`); // свой айди войса
+}
+
+var interval = setInterval(function () { change(); }, 2000000); // время обновления в миллисекундах
+
+client.login('OTkzMTUyNDY3ODI1MDY2MDY1.GnYIze.vXnePR-W0p5E7Sm6NTwua2hLDDnbOEAZu-Mpb4') // токен вашего бота
+
+// Хотите, чтобы ваш бот работал 24/7 бесплатно? Смотрите это видео: https://www.youtube.com/watch?v=wxdl4QK0am4
+
+// Bot by Sanich https://youtube.com/sanich - фишки, гайды по приложению Discord
